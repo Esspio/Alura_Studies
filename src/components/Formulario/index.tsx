29c289dc3c466cdd28116fad06564b4bf30384c0
Esspio <felipe.oliveira200085@gmail.com>
2024-06-02 18:17:52 -0300

    Incluindo state no Formulario

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,17 +3,39 @@ import React from 'react';
 import Botao from '../Botao';
 
 class Formulario extends React.Component {
+    state = {
+        nome: "",
+        tempo: "00:00"
+    };
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
+        evento.preventDefault();
+    }
+
     render(){
         return(
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor='tarefa'>Adicione um novo estudo</label>
-                    <input type='text' name='tarefa' id='tarefa' placeholder='O que deseja estudar?' required/>
+                    <input type='text'
+                     name='tarefa'
+                     id='tarefa'
+                     onChange={evento => this.setState({...this.state, nome: evento.target.value})}
+                     placeholder='O que deseja estudar?'
+                     required/>
 
                 </div>
                 <div className={style.inputContainer}>
                     <label htmlFor='tempo'>Tempo</label>
-					<input type='time' step='1' name='tempo' id='tempo' min='00:00:00' max='01:30:00' required/>
+					<input type='time'
+                     step='1'
+                     name='tempo'
+                     id='tempo'
+                     value={this.state.tempo}
+                     onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
+                     min='00:00:00'
+                     max='01:30:00'
+                     required/>
                 </div>
 
                 <Botao
